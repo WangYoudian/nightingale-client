a0d01c7ae6786c66bf9b8180473125c6060d9089
Bruce Wang <486590722@qq.com>
2020-06-04 14:36:50 +0800

    新增一些POST API测试

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -52,10 +52,92 @@ data = {
     "enable_etime": "23:59",
     "enable_days_of_week": [0, 1, 2, 3, 4, 5, 6],
     "need_upgrade": 0,
-    "id": 13
 }
 # openfalconclient.exceptions.InternalServerError: Get unknow error from falcon: (HTTP 500) (HTTP 500)
-# 没有这个id
+# 用try except进行捕捉
 # print(cli.api.portal.stra.put(data=data))  # Internal Server Error (HTTP 500)
 
-print(cli.api.portal.stra.get())
+print(cli.api.portal.stras.get())
+print(cli.api.portal.stras.effective.get())
+
+data = [
+  {
+    "type": "port",
+    "data": {
+      "nid": 2,
+      "collect_type": "port",
+      "name": "service.port",
+      "tags": "",
+      "port": 90,
+      "timeout": 3,
+      "step": 20,
+      "comment": ""
+    }
+  },
+  {
+    "type": "proc",
+    "data": {
+      "nid": 2,
+      "collect_type": "proc",
+      "name": "service",
+      "collect_method": "cmd",
+      "target": "tsdb",
+      "tags": "",
+      "step": 20,
+      "comment": ""
+    }
+  },
+  {
+    "type": "log",
+    "data": {
+      "nid": 2,
+      "collect_type": "log",
+      "func_type": "FLOW",
+      "name": "LOG.aa",
+      "func": "cnt",
+      "unit": "次数",
+      "file_path": "/home/xiaoju/alarm/log/DEBUG.log",
+      "time_format": "dd/mmm/yyyy:HH:MM:SS",
+      "step": 10,
+      "pattern": "flush"
+    }
+  }
+]
+print(cli.api.portal.collect.post(data=data))
+
+# api/index
+data = {
+    "endpoints": ["host1", "host2"],
+    "metrics": ["disk.used.percent"],
+}
+print(cli.api.index.tagkv.post(data=data))
+
+# api/transfer
+data = {
+  "start": 1562925134,
+  "end": 1562925234,
+  "series": [
+    {
+      "endpoints": [
+        "127.0.0.1",
+        "127.0.0.2"
+      ],
+      "metric": "proc.num",
+      "tagkv": [
+        {
+          "tagk": "target",
+          "tagv": [
+            "collector"
+          ]
+        },
+        {
+          "tagk": "service",
+          "tagv": [
+            "n9e-collector"
+          ]
+        }
+      ]
+    }
+  ]
+}
+print(cli.api.transfer.data.post(data=data))
